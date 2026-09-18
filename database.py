a23d4d1bf958341f4a0245be8cc80c0344efacdc
Debajyoti <debuhazra96@gmail.com>
2026-09-18 22:30:49 +0530

    Connect PostgreSQL

--- database.py
+++ database.py
@@ -1,33 +1,55 @@
-from sqlalchemy import Integer , Column , String , create_engine 
-from sqlalchemy.orm import DeclarativeBase , sessionmaker 
+import os
 
-# Create URL 
-DATABASE_URL="sqlite:///.SQL_DataBase.db"
+from sqlalchemy import Integer, Column, String, create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-# Create engine 
-engine = create_engine(url=DATABASE_URL , connect_args={'check_same_thread':False})
 
-# declare base 
+# Create URL
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///.SQL_DataBase.db"
+)
+
+
+# Create engine
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
+
+
+# Declare base
 class base(DeclarativeBase):
     pass
-# session create 
-session = sessionmaker(bind=engine , autoflush=False , autocommit=False)
 
-# Create DB 
+
+# Session create
+session = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
+
+
+# Create DB
 class QueryHistory(base):
     __tablename__ = "Database_FAST"
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     question = Column(String)
     limit = Column(Integer)
     answer = Column(String)
+
+
 base.metadata.create_all(bind=engine)
+
 
 def create_db():
     db = session()
-    try: 
-        yield db 
-    finally : 
+    try:
+        yield db
+    finally:
         db.close()
-    
-    
-    
